97bb109f689583e8889a5d73e3f6335cfdd49052
Senpothia <kintelalpz@gmail.com>
2019-12-21 18:41:53 +0100

    Ajout RTest: execution script R

---
+++ src/main/java/com/test/r/RTest6.java
@@ -0,0 +1,14 @@
+package com.test.r;
+
+import java.io.IOException;
+
+public class RTest6 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		  Runtime.getRuntime().exec("Rscript kaplanMeier.r");
+
+	}
+
+}
